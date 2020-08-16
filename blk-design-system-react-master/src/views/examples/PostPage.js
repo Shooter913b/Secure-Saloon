63e2d54d35aa18d300b5c0a1fa8c0b8831ba1cdb
Shooter913b <infant.elfrick@gmail.com>
2020-08-15 21:39:18 -0400

    Update PostPage.js

--- blk-design-system-react-master/src/views/examples/PostPage.js
+++ blk-design-system-react-master/src/views/examples/PostPage.js
@@ -145,6 +145,7 @@ class ProfilePage extends React.Component {
           <Card justify-content-md-center style={{height: '20rem'}}>
 
                 <CardBody>
+                <h3>Jhon Paul</h3>
                 <b>Everyone beware of the harsh weather! I heard there is gonna be some serious downpour! Get your raingear out.
                 :). Can't wait to see the weather with some hot chocolate!</b>
           </CardBody>
@@ -154,6 +155,7 @@ class ProfilePage extends React.Component {
               <Col lg="2" md="2">
                 <Card justify-content-md-center style={{height: '20rem'}}>
                       <CardBody>
+                      <h3>Liam Hefly</h3>
                       <b>We are having a yardsale! Trying to sell some stuff in case anyone is interested! Come by anytime
                       during the day! Feel free to look around too.</b>
                 </CardBody>
@@ -163,6 +165,7 @@ class ProfilePage extends React.Component {
                     <Col lg="2" md="2">
                       <Card justify-content-md-center style={{height: '20rem'}}>
                             <CardBody>
+                            <h3>lynda Bob</h3>
                             <b>Can anyone help me out with these logs? I can pay them $20 per hour to help me get these outta
                             the garage if anyone is interested.</b>
                       </CardBody>
@@ -172,6 +175,7 @@ class ProfilePage extends React.Component {
                           <Col lg="2" md="2">
                             <Card justify-content-md-center style={{height: '20rem'}}>
                                   <CardBody>
+                                  <h3>Bill Jobs</h3>
                                   <b>So excited for the neighborhood game today! Anyone else gonna come to the game? :)</b>
                             </CardBody>
                                 </Card>
