f4b870b4dc5fceb4fcd975f3afc3391db22186c1
Shooter913b <infant.elfrick@gmail.com>
2020-08-16 01:54:22 -0400

    done
    done

--- blk-design-system-react-master/src/views/examples/Map.js
+++ blk-design-system-react-master/src/views/examples/Map.js
@@ -1,53 +1,54 @@
 import React from 'react'
-import { GoogleMap, LoadScript,DrawingManager } from '@react-google-maps/api';
+import { GoogleMap, LoadScript, Marker, InfoWindow } from '@react-google-maps/api';
 
 const containerStyle = {
-  width: '1200px',
-  height: '800px'
+  width: '120vh',
+  height: '80vh'
 };
 
 const center = {
   lat:42.46,
   lng: -71.439
 };
-
-const onLoad = drawingManager => {
-  console.log(drawingManager)
+const position = {
+  lat: 42.46,
+  lng: -71.439
+}
+const onLoad = marker => {
+  console.log('marker: ', marker)
+}
+const divStyle = {
+  background: `white`,
+  border: `1px solid #ccc`,
+  padding: 15
 }
 
-const onPolygonComplete = polygon => {
-  console.log(polygon)
-}
 
 function MyComponent() {
-  const [map, setMap] = React.useState(null)
-
-  const onLoad = React.useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds();
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
-
-  const onUnmount = React.useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
   return (
     <LoadScript
       googleMapsApiKey="AIzaSyCStTMJl2QcIuEkTRGJyu-1TIO_46TcMaA"
     >
       <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={19}
-         id="drawing-manager-example"
+      id="marker-example"
+   mapContainerStyle={containerStyle}
+   zoom={19.5}
+   center={center}
       >
         { /* Child components, such as markers, info windows, etc. */ }
         <></>
-        <DrawingManager
+        <Marker
       onLoad={onLoad}
-      onPolygonComplete={onPolygonComplete}
+      position={position}
     />
+    <InfoWindow
+     onLoad={onLoad}
+     position={position}
+   >
+     <div style={divStyle}>
+       <h1>InfoWindow</h1>
+     </div>
+   </InfoWindow>
       </GoogleMap>
     </LoadScript>
   )
